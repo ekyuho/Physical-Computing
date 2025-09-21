f9fb616f1673384296988c16338b2250492fa9d9
Kyuho Kim <ekyuho@gmail.com>
2025-09-21 23:40:57 +0900

    Create blink-various-speed.ino

---
+++ blink-various-speed.ino
@@ -0,0 +1,57 @@
+/*
+  Blink
+
+  Turns an LED on for one second, then off for one second, repeatedly.
+
+  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
+  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
+  the correct LED pin independent of which board is used.
+  If you want to know what pin the on-board LED is connected to on your Arduino
+  model, check the Technical Specs of your board at:
+  https://docs.arduino.cc/hardware/
+
+  modified 8 May 2014
+  by Scott Fitzgerald
+  modified 2 Sep 2016
+  by Arturo Guadalupi
+  modified 8 Sep 2016
+  by Colby Newman
+
+  This example code is in the public domain.
+
+  https://docs.arduino.cc/built-in-examples/basics/Blink/
+*/
+
+#define LED_BUILTIN 2
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(115200);
+}
+
+int i=6;
+int n=0;
+int t[10]={10,25,50,100,200,500,1000};
+
+// the loop function runs over and over again forever
+void loop() {
+  if (n==0) {
+    n=5;
+    for (int k=0;k<10;k++) {
+      digitalWrite(LED_BUILTIN, HIGH);
+      delay(20);
+      digitalWrite(LED_BUILTIN, LOW);
+      delay(20);
+    }
+    i++;
+    if (i==7) i=0;
+  }
+  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+  Serial.println(t[i]);
+  delay(t[i]);  
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
+  delay(2000);  
+  n--;
+}
