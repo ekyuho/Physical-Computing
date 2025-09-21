3d9e582de6733e44c69768c4e1bfe167df74650b
Kyuho Kim <ekyuho@gmail.com>
2025-09-22 01:04:46 +0900

    Create led-three-5.ino

---
+++ led-three-5.ino
@@ -0,0 +1,40 @@
+void setup() {
+  pinMode(2, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(14, OUTPUT);
+  pinMode(15, OUTPUT);
+
+  digitalWrite(12, LOW);
+  digitalWrite(14, LOW);
+  digitalWrite(15, LOW);
+}
+
+void loop() {
+  digitalWrite(2, HIGH);
+  delay(50);
+  digitalWrite(2, LOW);
+
+  digitalWrite(4, HIGH);
+  delay(50);
+  digitalWrite(4, LOW);
+
+  digitalWrite(5, HIGH);
+  delay(50);
+  digitalWrite(5, LOW);
+  delay(1000);    
+
+  digitalWrite(5, HIGH);
+  delay(50);
+  digitalWrite(5, LOW);
+
+  digitalWrite(4, HIGH);
+  delay(50);
+  digitalWrite(4, LOW);
+
+  digitalWrite(2, HIGH);
+  delay(50);
+  digitalWrite(2, LOW);
+  delay(1000); 
+}
